467946093f3a07e4bc27e1633229dfa7462b7e1c
Thai Duy <chestraxx@gmail.com>
2023-08-23 19:52:59 +0700

    commit nodejs globals

---
+++ node/2-globals.js
@@ -0,0 +1,12 @@
+// GLOBALS - NO WINDOW
+
+// __dirname  - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module  - infor about current module (file)
+// process - info about env where the program is being executed
+
+console.log("__dirname", __dirname);
+console.log("__filename", __filename);
+console.log("module", module);
+console.log("process", process);
